815581d002249835e6d34955e9127fd6e1164118
John Ang <johnang@berkeley.edu>
2017-01-10 15:04:09 -0800

    added error page

--- Analyzer/config/routes.rb
+++ Analyzer/config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+
+  get 'errors/internal_server_error'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'searches/new'
   root 'searches#new'
   resources :searches
   get 'searches/words/:id', to: 'searches#words', as: "searches_words"
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
